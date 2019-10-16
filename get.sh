a7e310f41a247e4a2194b1def44c7a49486c5597
Wenwei Chung <wenwei.chung@gmail.com>
2019-10-17 00:23:34 +0800

    modified get.sh

--- get.sh
+++ get.sh
@@ -13,12 +13,31 @@ echo $current_date_time;
 
 cd /home/pi/Downloads/
 
-/home/pi/miniconda3/envs/py36/bin/python -u /home/pi/Downloads/捉取技術指標.py > 技術.log &
+/home/pi/miniconda3/envs/py36/bin/python -u /home/pi/Downloads/捉取技術指標.py > 捉取日技術指標.log &&
+echo "捉取技術指標.py is done";
 
-echo "python -u 捉取技術指標.py > 技術.log & \a \n"
+/home/pi/miniconda3/envs/py36/bin/python -u /home/pi/Downloads/捉取卷商買賣.py > 捉取卷商買賣.log &&
+echo "捉取卷商買賣.py is done";
 
-/home/pi/miniconda3/envs/py36/bin/python -u /home/pi/Downloads/捉取卷商買賣.py > 卷商.log &
+/home/pi/miniconda3/envs/py36/bin/python -u /home/pi/Downloads/捉取周技術指標.py > 捉取周技術指標.log &&
+echo "捉取周技術指標.py is done";
 
-echo "python -u 捉取卷商買賣.py > 卷商.log & \a \n"
+/home/pi/miniconda3/envs/py36/bin/python -u /home/pi/Downloads/捉取3大法人持股.py > 捉取3大法人持股.log &&
+echo "捉取3大法人持股.py is done";
+
+/home/pi/miniconda3/envs/py36/bin/python -u /home/pi/Downloads/捉取借還卷.py > 捉取借還卷.log &&
+echo "捉取借還卷.py is done";
+
+/home/pi/miniconda3/envs/py36/bin/python -u /home/pi/Downloads/捉取融資融卷.py > 捉取融資融卷.log &&
+echo "捉取融資融卷.py is done";
+
+/home/pi/miniconda3/envs/py36/bin/python -u /home/pi/Downloads/寫入技術指標周.py > 寫入技術指標周.log &&
+echo "寫入周技術指標.py is done";
+
+/home/pi/miniconda3/envs/py36/bin/python -u /home/pi/Downloads/捉取卷商買賣.py > 捉取卷商買賣.log &&
+echo "捉取卷商買賣.py is done";
+
+/home/pi/miniconda3/envs/py36/bin/python /home/pi/Downloads/upload.py &&
+echo "upload.py is done";
 
 exit 0
